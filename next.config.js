f1b412afddf6407652fe2f9dc261aef02ba4a7bf
varekai-dev <serhijsav@gmail.com>
2022-06-02 23:29:53 +0300

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	poweredByHeader: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `https://netflix-online-cinema-backend.herokuapp.com/api/:path*`,
+			},
+			{
+				source: '/uploads/:path*',
+				destination: `https://netflix-online-cinema-backend.herokuapp.com/uploads/:path*`,
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
